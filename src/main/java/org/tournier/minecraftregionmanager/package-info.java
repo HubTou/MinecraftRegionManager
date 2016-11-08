f835e509d917a5538261c10b9732a3a1c98b82bc
Hubert <github@tournier.info>
2016-11-08 17:52:40 +0100

    Manual commit to create the directory

---
+++ src/main/java/org/tournier/minecraftregionmanager/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * A Minecraft Bukkit plugin intended to ease builds backup/restore during map resets. 
+ * <p>
+ * The first release provides all the marking functions.
+ *
+ * @author HubTou
+ * @version 0.9
+ */
+package org.tournier.minecraftregionmanager;
