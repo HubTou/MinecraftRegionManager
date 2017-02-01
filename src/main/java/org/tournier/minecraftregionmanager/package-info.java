5402235fa0bae2023aeca06299318f240313136b
Hubert <github@tournier.info>
2017-02-01 19:30:27 +0100

    Update to v1.00

--- src/main/java/org/tournier/minecraftregionmanager/package-info.java
+++ src/main/java/org/tournier/minecraftregionmanager/package-info.java
@@ -1,9 +1,20 @@
 /**
  * A Minecraft Bukkit plugin intended to ease builds backup/restore during map resets. 
  * <p>
- * The first release provides all the marking functions.
+ * This version is the first one to include backup/restore functions (map reset).
  *
  * @author HubTou
- * @version 0.91
+ * @version 1.00
  */
 package org.tournier.minecraftregionmanager;
+
+/**
+ * 1.00    2017-01-25  Added the /resetmap command for resetting dimensions to marked items
+ *                     Added a /verifyregion command for checking region files integrity
+ *                     Modified the /showlocation command to indicate if a region/chunk is marked
+ *                     Modified the /markarea command to say something after first corner marking
+ * 0.91    2016-11-09  Added a config.yml file for admin modifiable configuration
+ *                     Added a /unmarkeverything command for testing players
+ *                     Added a /listreports command for admins
+ * 0.90    2016-11-08  Initial public release
+ */
