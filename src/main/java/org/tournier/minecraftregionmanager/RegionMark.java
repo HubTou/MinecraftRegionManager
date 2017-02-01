f19f66c1c1fb175633a90419b13be193e7643ed2
Hubert <github@tournier.info>
2017-02-01 19:35:21 +0100

    Update to v1.00

--- src/main/java/org/tournier/minecraftregionmanager/RegionMark.java
+++ src/main/java/org/tournier/minecraftregionmanager/RegionMark.java
@@ -2,42 +2,50 @@ package org.tournier.minecraftregionmanager;
 
 import java.io.File;
 
-public class RegionMark {
+public class RegionMark
+{
 	// A region mark is defined by 4 items:
 	private String worldName;
 	private int x, z;
 	private String regionName;
 	
 	// Constructor
-    public RegionMark(String world, int rx, int rz, String name) {
+    public RegionMark(String world, int rx, int rz, String name)
+    {
         worldName = world;
         x = rx;
         z = rz;
         regionName = name;
     }
     
-    public String getWorldName() {
+    public String getWorldName()
+    {
     	return worldName;
     }
     
-    public int getX() {
+    public int getX()
+    {
     	return x;
     }
     
-    public int getZ() {
+    public int getZ()
+    {
     	return z;
     }
     
-    public String getRegionName() {
+    public String getRegionName()
+    {
     	return regionName;
     }
     
-    public File getFileName() {
+    public File getFileName()
+    {
     	File f = new File("./" + worldName + "/r." + x + "." + z + ".mca");
     	return f;
 	}
     
-    public String printMark() {
+    public String printMark()
+    {
     	String s = worldName + ":" + x + "," + z + ":" + regionName + " (blocks " + (x*16*32) + "," + (z*16*32) + " to " + ((x*16*32)+511) + "," + ((z*16*32)+511) + ")";
     	return s;
     }
